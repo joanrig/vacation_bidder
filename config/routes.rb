f0043d63a3ad4a42222b7ba9660559f3f10e5553
joanrig <github email address>
2019-05-14 02:12:45 -0400

    made bucket_list_items a nested route under travelers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :vacations
-  resources :bucket_list_items
   resources :attractions
   resources :destinations
   resources :categories
   resources :travelers
+    resources :bucket_list_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
