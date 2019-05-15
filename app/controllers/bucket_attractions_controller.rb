3683ce72c9b36411fc286f11403e5f9815a8a6b3
joanrig <github email address>
2019-05-15 16:26:00 -0400

    can now load page to add attractions to bucket list

--- app/controllers/bucket_attractions_controller.rb
+++ app/controllers/bucket_attractions_controller.rb
@@ -5,15 +5,16 @@ class BucketAttractionsController < ApplicationController
     @item = BucketListItem.find_by_id(params[:bucket_list_item_id])
     @attractions = Attraction.all
     @bucket_attraction = @item.bucket_attractions.build(bucket_list_item_id: @item.id)
-    #figure out how to call it from another controller
   end
 
   def create
+    binding.pry
     @item = BucketListItem.find_by_id(params[:bucket_list_item_id])
     @attraction = Attraction.find_by_id(params[:attraction_id])
 
-    @item.bucket_attractions.build(attraction_id: @attraction)
+    @bucket_attraction = BucketAttraction.create(bucket_list_item_id: @item.id, attraction_id: @attraction.id)
     binding.pry
+
     if @item.save
       redirect_to bucket_list_item_path(@item)
     else
