a1b86bdfb93ed3b0c9aeafe75f0bfc18a430aed7
joanrig <github email address>
2019-05-15 20:43:36 -0400

    can now make and view bucket attractions

--- app/controllers/bucket_attractions_controller.rb
+++ app/controllers/bucket_attractions_controller.rb
@@ -8,22 +8,25 @@ class BucketAttractionsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = BucketListItem.find_by_id(params[:bucket_list_item_id])
-    @attraction = Attraction.find_by_id(params[:attraction_id])
-    @bucket_attraction = BucketAttraction.create(bucket_list_item_id: @item.id, attraction_id: @attraction.id)
+    @attraction = Attraction.find_by_id(params[:bucket_attraction][:attraction_id])
+    binding.pry
+    @bucket_attraction = BucketAttraction.new(bucket_list_item_id: @item.id, attraction_id: @attraction.id)
 
-
-    if @item.save
+    if @bucket_attraction.save
       redirect_to bucket_list_item_path(@item)
     else
-      flash[:alert] = @item.errors.full_messages
+      flash[:alert] = @bucket_attraction.errors.full_messages
       render :new
     end
-
-    binding.pry
-    #grab id from params, grab user id to create this new bucket attraction,
   end
+
+    private
+
+    def bucket_attraction_params
+      params.require(:bucket_attraction).permit(:attraction_id)
+    end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :vacations
+
   resources :bucket_list_items do
     resources :bucket_attractions, only: [:new, :create]
   end
