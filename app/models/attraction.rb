ccbf4fdd9187850f71e906cdd5e3368609d1a347
joanrig <github email address>
2019-05-16 09:36:28 -0400

    updated relationships on attraction model

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,8 +1,9 @@
 class Attraction < ApplicationRecord
-  has_many :bucket_attractions
-  has_many :bucket_list_items, through: :bucket_attractions
-  has_many :users, through: :bucket_list_items
+  has_many :vacation_attractions
+  has_many :vacations, through: :vacation_attractions
+
   has_many :attraction_categories
   has_many :categories, through: :attraction_categories
+  
   validates :name, presence: true, uniqueness: true
 end
