7493a75618e8857ce9186ca0ab776e677ee75199
joanrig <github email address>
2019-05-16 09:30:43 -0400

    updated relationships on bucket list item model

--- app/models/bucket_list_item.rb
+++ app/models/bucket_list_item.rb
@@ -1,9 +1,6 @@
 class BucketListItem < ApplicationRecord
   belongs_to :user
   belongs_to :vacation, optional: true
-  has_many :bucket_attractions
-  accepts_nested_attributes_for :bucket_attractions
-  has_many :attractions, through: :bucket_attractions
   validates :title, presence: true, uniqueness: true
 
   def attractions_names
