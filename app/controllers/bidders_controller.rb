84df18457274d399ee62eba1dd6411cfe1bc7bd7
joanrig <github email address>
2019-05-17 16:35:44 -0400

    building out bid views

---
+++ app/controllers/bidders_controller.rb
@@ -0,0 +1,50 @@
+class BiddersController < ApplicationController
+
+
+
+  def new
+    @bidder = Bidder.new
+  end
+
+  def create
+    @bidder = Bidder.new(bidder_params)
+    current_user.update(role: "Bidder")
+    if @bidder.save
+      redirect_to bidder_path(@bidder)
+    else
+      flash[:alert] = @bidder.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @bidder = Bidder.find_by_id(params[:id])
+  end
+
+
+  def edit
+    @bidder = Bidder.find_by_id(params[:id])
+  end
+
+  def update
+    @bidder = Bidder.find_by_id(params[:id])
+    @bidder.update(bidder_params)
+    if @bidder.save
+      flash[:notice] = "Successfully updated."
+      redirect_to bidder_path(@bidder)
+    else
+      flash[:alert] = @bidder.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @bidder = Bidder.find_by_id(params[:id])
+    @bidder.destroy
+  end
+
+  private
+    def bidder_params
+      params.require(:bidder).permit(:name, :email, :website)
+    end
+end
