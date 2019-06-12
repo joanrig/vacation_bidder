30ab966e84066a45a5a853bce32ac3f5228e5ea6
github username <github email address>
2019-06-12 18:47:03 -0400

    working in atom again, fixed indentation for atom, can't work in vs code on macbook pro 2019

--- app/assets/javascripts/vacations.js
+++ app/assets/javascripts/vacations.js
@@ -13,63 +13,63 @@ function listenForClick() {
 }
 
 function getVacations() {
-    $.ajax({
-        url: 'http://localhost:3000/vacations',
-        method: 'get',
-        dataType: 'json',
-        success: function(data) {
-            data.map(vacation => {
-                // new js object 
-                const newVacation = new Vacation(vacation)
-                const newVacationHtml = newVacation.vacationHTML()
-                document.getElementById('ajax-vacations').innerHTML += newVacationHtml
-                listenForHover()
-            })
-        }
-    })
+  $.ajax({
+      url: 'http://localhost:3000/vacations',
+      method: 'get',
+      dataType: 'json',
+      success: function(data) {
+        data.map(vacation => {
+          // new js object
+          const newVacation = new Vacation(vacation)
+          const newVacationHtml = newVacation.vacationHTML()
+          document.getElementById('ajax-vacations').innerHTML += newVacationHtml
+          listenForHover()
+      })
+    }
+  })
 }
 
 function listenFornewVacationFormClick() {
-    $('button#ajax-new-vacation').on('click', function(event) {
-        event.preventDefault()
-        let newVacationForm = Vacation.newVacationForm()
-            // $('div#new-vacation-form-div')
-        document.querySelector('div#new-vacation-form-div').innerHTML = newVacationForm
+  $('button#ajax-new-vacation').on('click', function(event) {
+      event.preventDefault()
+      let newVacationForm = Vacation.newVacationForm()
+          // $('div#new-vacation-form-div')
+      document.querySelector('div#new-vacation-form-div').innerHTML = newVacationForm
 
-    })
+  })
 }
 
 const allVacations = []
 
 class Vacation {
-    constructor(obj) {
-        this.id = obj.id
-        this.name = obj.name
-        this.category = obj.category
-        this.budget = obj.budget
-        this.number_of_travelers = obj.number_of_travelers
-        this.notes = obj.notes
-        this.open_to_bids = obj.open_to_bids
-        this.attractions = obj.attractions
-        allVacations.push(this)
-    }
+  constructor(obj) {
+    this.id = obj.id
+    this.name = obj.name
+    this.category = obj.category
+    this.budget = obj.budget
+    this.number_of_travelers = obj.number_of_travelers
+    this.notes = obj.notes
+    this.open_to_bids = obj.open_to_bids
+    this.attractions = obj.attractions
+    allVacations.push(this)
+  }
 
-    static newVacationForm() {
-        return (`
-		<strong>New vacation form</strong>
-			<form>
-				<input id='vacation-name' type='text' name='name'></input><br>
-                <input type='text' name='budget'></input><br>
-                <input type='text' name='number_of_travelers'></input><br>
-                <input type='text' name='notes'></input><br>    
-				<input type='submit' />
-			</form>
-		`)
-    }
+  static newVacationForm() {
+    return (`
+    	<strong>New vacation form</strong>
+    		<form>
+    			<input id='vacation-name' type='text' name='name'></input><br>
+                  <input type='text' name='budget'></input><br>
+                  <input type='text' name='number_of_travelers'></input><br>
+                  <input type='text' name='notes'></input><br>
+    			<input type='submit' />
+    		</form>
+	  `)
+  }
 }
 
 Vacation.prototype.vacationHTML = function() {
-    return (`	
+    return (`
 		<div class='vacation-js' id="${this.id}">
       <h3>${this.name}</h3>
 		</div>
@@ -77,22 +77,31 @@ Vacation.prototype.vacationHTML = function() {
 }
 
 function listenForHover() {
-    $('div.vacation-js').mouseenter(function(event) {
-        event.preventDefault()
-        const id = event.target.parentElement.id
+  $('div.vacation-js').mouseenter(function(event) {
+    event.preventDefault()
+    const id = event.target.parentElement.id
 
-        // this doesn't work: 
-        // const found = allVacations.find(v => v.id === id)
-        const found = allVacations.find(v => v.id === 1)
-
-        const attractionsHTML = (`	
-		    <div class='vacation_attractions'>
-                <h3>${found.attractions}</h3>
-		    </div>
+    // this doesn't work:
+    // const found = allVacations.find(v => v.id === id)
+    const found = allVacations.find(v => v.id === 1)
+    const attractionsHTML = found.attractions.map {
+      attraction => {
+        return (`
+          <div class='attractions'>
+              <h3>${attraction.name}</h3>
+          </div>
         `)
+      }
+    }
 
-        // trying to add above html to DOM
-        $('div.vacation-js').innerHTML += attractionsHTML
-        debugger
-    })
+    const attractionsHTML = (`
+      <div class='vacation_attractions'>
+          <h3>${found.attractions}</h3>
+      </div>
+    `)
+
+    // trying to add above html to DOM. no errors, but nothing happens
+    $('div.vacation-js#found.id').innerHTML += attractionsHTML
+    debugger
+  }
 }
