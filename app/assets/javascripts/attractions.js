33a0064e0a738da67ed50723ec331a753f96d053
github username <github email address>
2019-06-14 18:17:35 -0400

    notes form pre-populates with current notes

--- app/assets/javascripts/attractions.js
+++ app/assets/javascripts/attractions.js
@@ -9,11 +9,14 @@ $(document).ready(function() {
   }
 
   function notesForm (){
+    const currentComments = document.querySelector('.current-comments').innerText
     return `
       <form action="/attractions" method="post">
-          <input type="text" name="attraction[notes]" value="">
+          <input type="text" name="attraction[notes]" value="${currentComments}">
       </form>
     `
   }
+
+
 
 })
