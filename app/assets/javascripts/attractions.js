966ddaa6ed3ce615e7a18e38a4258b09f6b136ab
github username <github email address>
2019-06-14 03:17:20 -0400

    fixed brackets can now hit debugger

--- app/assets/javascripts/attractions.js
+++ app/assets/javascripts/attractions.js
@@ -1,46 +1,43 @@
 $(document).ready(function() {
-  setEventListener()
+  getAttractions()
+  updateNotes()
 
-  // function getAttraction(){
-  //   const id = document.URL.split('/')[4]
-  //   $.ajax({
-  //       url: `http://localhost:3000/attractions/:${id}`,
-  //       method: 'get',
-  //       dataType: 'json',
-  //       success: function(data) {data.map
-  //         debugger
-  //         (attraction => {
-  //           // new js object
-  //           const newAttraction = new Attraction(attraction)
-  //         })
-  //       }
-  //   })
-  // }
-  //
-  // class Attraction {
-  //     constructor(obj) {
-  //         this.id = obj.id
-  //         this.name = obj.name
-  //         this.website = obj.website
-  //         this.city = obj.city
-  //         this.state = obj.state
-  //         this.county = obj.county
-  //         this.country = obj.country
-  //         this.notes = obj.notes
-  //         this.public = obj.public
-  //     }
-  //   }
-
-  function setEventListener(){
-    debugger
-    document.querySelector('.glyphicon').addEventListener("click", addOrEditNotes)
+  function getAttractions() {
+    const id = document.URL.split('/')[4]
+    $.ajax({
+        url: 'http://localhost:3000/attractions',
+        method: 'get',
+        dataType: 'json',
+        success: function(data) {data.map(attraction => {
+            // new js object
+            const newAttraction = new Attraction(attraction)
+          })
+        }
+    })
   }
 
-  function addOrEditNotes() {
-    debugger
+  class Attraction {
+      constructor(obj) {
+          this.id = obj.id
+          this.name = obj.name
+          this.website = obj.website
+          this.city = obj.city
+          this.state = obj.state
+          this.county = obj.county
+          this.country = obj.country
+          this.notes = obj.notes
+          this.public = obj.public
+      }
+    }
+
+  function updateNotes(){
+    document.querySelector('.glyphicon').addEventListener("click", function(){
+      const id = document.URL.split('/')[4]
+      debugger
+    })
   }
 
-  function showNotes(){
-  }
+
+
 
 })
