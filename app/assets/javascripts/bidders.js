03ac3426cd62e9b2517809a03d325145f70b0082
github username <github email address>
2019-06-16 15:26:19 -0400

    form working again, barebones js

--- app/assets/javascripts/bidders.js
+++ app/assets/javascripts/bidders.js
@@ -1,4 +1,36 @@
-
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed')
+//
+//
+//
+// class Bidder {
+//     constructor(obj) {
+//         this.id = obj.id
+//         this.name = obj.name
+//         this.email = obj.email
+//         this.website = obj.website
+//     }
+//
+//
+//     static newBidderForm() { 
+//       return (` 
+//         <form id="bidder-form"> 
+//         <input id='name' type='text' name='name' placeholder="Company Name"></input><br> 
+//         <input id='email' type='text' name='email' placeholder="Email"></input><br> 
+//         <input id='website' type='text' name='website' placeholder="Website"></input><br> 
+//         <input type='submit'/> 
+//         </form> 
+//         `) 
+//       }
+//   }
+//
+//   Bidder.prototype.bidderHTML = function () {
+//     return (`
+//       <p>${data.name}</p>
+//       <p>${data.email}</p>
+//       <p>${data.website}</p>
+//     `)
+//     }
 
 $(function () {
   $('form').submit(function(event) {
@@ -14,4 +46,7 @@ $(function () {
       $("#website").text(post["website"])
     })
   })
+})
+
+//document ready
 })
