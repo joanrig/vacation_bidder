39934d71ccf938312c339c833db8fae984a9cf1c
github username <github email address>
2019-06-16 04:57:48 -0400

    added attractions.js, app completely ignores js

---
+++ app/assets/javascripts/attractions.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  function submitForm () {
+    debugger
+    $('form').submit(function(event) {
+      event.preventDefault()
+
+      const values = $(this).serialize()
+      const posting = $.post('/attractions', values)
+
+      posting.done(function(data) {
+        showAttraction(data)
+      })
+    }
+  })
+
+  function showAttraction(data){
+    let categories = data.categories.map(c => {
+      return(`
+          <a href="http://localhost3000/categories/${c.id}">${c.name}</a>
+          <br>
+      `)
+
+      $('#attraction-container').innerHTML = `
+        <h1>Name: ${data.name}</h1>
+        <p>Categories: ${categories}</p>
+      `
+    })
+  }
+
+
+
+
+//document ready
+})
