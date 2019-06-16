a8e36018e2b60bf9909476d9db22ae14c0559efe
github username <github email address>
2019-06-16 04:06:03 -0400

    disabled bidder.js; bucket list item new works again, but ignores bucket list item js

--- app/assets/javascripts/bidders.js
+++ app/assets/javascripts/bidders.js
@@ -1,17 +1,17 @@
 
 
-$(function () {
-  $('form').submit(function(event) {
-    event.preventDefault()
-
-    const values = $(this).serialize()
-    const posting = $.post('/bidders', values)
-
-    posting.done(function(data) {
-      const post = data
-      $("#name").text(post["name"])
-      $("#email").text(post["email"])
-      $("#website").text(post["website"])
-    })
-  })
-})
+// $(function () {
+//   $('form').submit(function(event) {
+//     event.preventDefault()
+//
+//     const values = $(this).serialize()
+//     const posting = $.post('/bidders', values)
+//
+//     posting.done(function(data) {
+//       const post = data
+//       $("#name").text(post["name"])
+//       $("#email").text(post["email"])
+//       $("#website").text(post["website"])
+//     })
+//   })
+// })
--- app/assets/javascripts/bucket_list_items.js
+++ app/assets/javascripts/bucket_list_items.js
@@ -1,61 +1,61 @@
 $(document).ready(function() {
-  listenFornewBucketListItemFormClick()
-})
+  listenForNewBucketListItemFormClick()
 
 
-function listenFornewBucketListItemFormClick() {
-    $('button#new-bucket-list-item').on('click', function(event) {
-        event.preventDefault()
-        let newBucketListItemForm = BucketList.newBucketListItemForm()
-            // $('div#new-vacation-form-div')
-        document.querySelector('div#new-bucket-list-item-form').innerHTML = newBucketListItemForm()
+  function listenForNewBucketListItemFormClick() {
+      $('#new-bucket-list-item').on('click', function(event) {
+          event.preventDefault()
+          let newBucketListItemForm = BucketListItem.newBucketListItemForm()
+          debugger
+          document.querySelector('div#new-bucket-list-item-form').innerHTML = newBucketListItemForm()
 
-    })
-}
-
-
-const allBucketListItems = []
-
-class BucketListItem {
-    constructor(obj) {
-        this.id = obj.id
-        this.name = obj.name
-        this.user = obj.user
-        this.notes = obj.notes
-        this.vacation = obj.vacation
-        allBucketListItems.push(this)
-    }
+      })
   }
 
+  const allBucketListItems = []
 
+  class BucketListItem {
+      constructor(obj) {
+          this.id = obj.id
+          this.name = obj.name
+          this.user = obj.user
+          this.notes = obj.notes
+          this.vacation = obj.vacation
+          allBucketListItems.push(this)
+      }
 
-  static newBucketListItemForm() {
-      return (`
-        <%= form_for bucket_list_item do |f| %>
-        <div class="field<%= ' field_with_errors' if bucket_list_item.errors[:name].any? %>">
-          <div class="center" style="text-align:center">
-          <%= f.label :name, "Name of bucket list item" %><br>
-          <%= f.text_field :name %>
-          </div>
-        </div>
-
-        <br>
-        <br>
-        <div class="field<%= ' field_with_errors' if bucket_list_item.errors[:notes].any? %>">
-          <div class="center" style="text-align:center">
-            <%= f.label :notes %><br>
-            <div style="color:black">
-              <%= f.text_area :notes %>
+      static newBucketListItemForm() {
+        debugger
+        return (`
+          <%= form_for bucket_list_item do |f| %>
+          <div class="field<%= ' field_with_errors' if bucket_list_item.errors[:name].any? %>">
+            <div class="center" style="text-align:center">
+            <%= f.label :name, "Name of bucket list item" %><br>
+            <%= f.text_field :name %>
             </div>
           </div>
-        </div>
 
-        <br>
-        <br>
-        <%= f.hidden_field :user_id, :value => user.id %>
-        <div style="color:black">
-          <%= f.submit %>
-        </div>
-        <% end %>
-      `)
-  }
+          <br>
+          <br>
+          <div class="field<%= ' field_with_errors' if bucket_list_item.errors[:notes].any? %>">
+            <div class="center" style="text-align:center">
+              <%= f.label :notes %><br>
+              <div style="color:black">
+                <%= f.text_area :notes %>
+              </div>
+            </div>
+          </div>
+
+          <br>
+          <br>
+          <%= f.hidden_field :user_id, :value => user.id %>
+          <div style="color:black">
+            <%= f.submit %>
+          </div>
+          <% end %>
+        `)
+      }
+    }
+
+//document ready
+  })
